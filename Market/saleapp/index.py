76d16ff067b222eded01c5a1ad9d6c914b9f90f2
MaiNgoc <mvn.bp01@gmail.com>
2024-03-23 16:35:20 +0700

    update

--- Market/saleapp/index.py
+++ Market/saleapp/index.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import render_template
+from saleapp import dao
+
+from saleapp import app
+
+
+@app.route("/")
+def index():
+    categories = dao.load_categories()
+    products = dao.load_products()
+
+    return render_template('index.html', categories=categories,
+                           products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
